41fc2ce1576331895e72b01053e718b619828e07
sephadon <websworld@bbox.fr>
2025-03-24 10:29:35 +0100

    Limit the number of requests

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 
 use App\Repository\CardRepository;
 use App\Service\DealTheCards;
-use Doctrine\Migrations\Configuration\Migration\JsonFile;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -16,8 +19,13 @@ class CardController extends AbstractController
 {
     // Retrieve all cards
     #[Route('/api-pth/cards', name: 'card', methods: ['GET'])]
-    public function getCards(CardRepository $cardRepository, SerializerInterface $serializer): JsonResponse
+    public function getCards(Request $request, RateLimiterFactory $anonymousApiLimiter, CardRepository $cardRepository, SerializerInterface $serializer): JsonResponse
     {
+        $limiter = $anonymousApiLimiter->create($request->getClientIp());
+        if (false === $limiter->consume(1)->isAccepted()) {
+            throw new TooManyRequestsHttpException(JsonResponse::HTTP_BAD_REQUEST, "Vous avez dépassé le nombre de requêtes autorisées");
+        }
+
         $cardList = $cardRepository->findAll();
         $jsonCardList = $serializer->serialize($cardList, 'json');
         return new JsonResponse($jsonCardList, Response::HTTP_OK, [], true);
@@ -25,7 +33,12 @@ class CardController extends AbstractController
 
     // Retrieve a set
     #[Route('/api-pth/game/{nbrPlayers}', name: 'game', methods: ['GET'])]
-    public function game(int $nbrPlayers,CardRepository $cardRepository, SerializerInterface $serializer): JsonResponse {
+    public function game(Request $request, RateLimiterFactory $anonymousApiLimiter, int $nbrPlayers,CardRepository $cardRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $limiter = $anonymousApiLimiter->create($request->getClientIp());
+        if (false === $limiter->consume(1)->isAccepted()) {
+            throw new TooManyRequestsHttpException(JsonResponse::HTTP_BAD_REQUEST, "Vous avez dépassé le nombre de requêtes autorisées");
+        }
 
         if ($nbrPlayers < 2 || $nbrPlayers > 6) {
             throw new HttpException(JsonResponse::HTTP_BAD_REQUEST, "Le nombre de joueurs doit être compris entre 2 et 6");
